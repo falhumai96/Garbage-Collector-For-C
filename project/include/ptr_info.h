6e4f6c9ab707a99524b11f9f51bc9d3f5051f0a8
Faisal Al-Humaimidi <falhumai@sfu.ca>
2018-08-18 23:16:03 -0700

    Implement "ptr_info".

--- project/include/ptr_info.h
+++ project/include/ptr_info.h
@@ -6,9 +6,10 @@ typedef struct _ptr_info_t {
 } ptr_info_t;
 
 ptr_info_t* create_ptr_info();
+ptr_info_t* create_ptr_info_with_arg(void* ptr);
 ptr_info_t* clone_ptr_info(ptr_info_t* this_ptr_info);
 void        del_ptr_info(ptr_info_t* this_ptr_info);
-void        set_ptr_in_ptr_info(ptr_info_t* this_ptr_info);
+void        set_ptr_in_ptr_info(ptr_info_t* this_ptr_info, void* ptr);
 void*       get_ptr_in_ptr_info(ptr_info_t* this_ptr_info);
 
 #endif
--- project/lib/ptr_info.c
+++ project/lib/ptr_info.c
@@ -3,21 +3,28 @@
 #include "ptr_info.h"
 
 ptr_info_t* create_ptr_info() {
-    return NULL;
+    return create_ptr_info_with_arg(NULL);
+}
+
+ptr_info_t* create_ptr_info_with_arg(void* ptr) {
+    ptr_info_t* to_return = (ptr_info_t*) malloc(sizeof(ptr_info_t));
+    to_return->ptr = ptr;
+    return to_return;
 }
 
 ptr_info_t* clone_ptr_info(ptr_info_t* this_ptr_info) {
-    return NULL;
+    return create_ptr_info_with_arg(this_ptr_info->ptr);
 }
 
 void del_ptr_info(ptr_info_t* this_ptr_info) {
-
+    // Should I delete the actual pointer?
+    free(this_ptr_info);
 }
 
-void set_ptr_in_ptr_info(ptr_info_t* this_ptr_info) {
-
+void set_ptr_in_ptr_info(ptr_info_t* this_ptr_info, void* ptr) {
+    this_ptr_info->ptr = ptr;
 }
 
 void* get_ptr_in_ptr_info(ptr_info_t* this_ptr_info) {
-    return NULL;
+    return this_ptr_info->ptr;
 }
